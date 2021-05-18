ed3ebd080b03832ea0f46200302d347159074c2d
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-05-18 12:35:57 +0200

    fin

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './sass/main.scss';
+  
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+
+const bodyBgColor = () => {
+    document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let countValue = null;
+
+startBtn.addEventListener('click', event => {
+    countValue = setInterval(bodyBgColor, 1000);
+    event.target.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(countValue)
+    startBtn.disabled = false;
+});
